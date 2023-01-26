3a8a50d36af371f57b2a3640f0f1582eff606213
Abdullah Zafar <zafar.abdullah10@gmail.com>
2023-01-26 13:06:59 -0500

    update

---
+++ app-video/src/components/Video.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Video (props) {
+  return (
+        <div className="card text-white bg-secondary mb-3 style-note">
+            <div className="card-header">{props.head}</div>
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.content}</p>
+            </div>
+        </div>
+  )
+}
+
+export default Video
